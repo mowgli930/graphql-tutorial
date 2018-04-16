745314c93c54001dcadb99e300ace9bbf41c3b61
leo <leo.moralesvega@gmail.com>
2018-04-16 20:17:32 +0200

    initial commit

---
+++ src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -0,0 +1,25 @@
+package com.howtographql.hackernews;
+
+import com.coxautodev.graphql.tools.SchemaParser;
+import javax.servlet.annotation.WebServlet;
+
+import graphql.schema.GraphQLSchema;
+import graphql.servlet.SimpleGraphQLServlet;
+
+
+@WebServlet(urlPatterns = "/graphql")
+public class GraphQLEndpoint extends SimpleGraphQLServlet {
+
+    public GraphQLEndpoint() {
+        super(buildSchema());
+    }
+
+    public static GraphQLSchema buildSchema() {
+        LinkRepository linkRepository = new LinkRepository();
+        return SchemaParser.newParser()
+                .file("schema.graphqls") //parse the schema file created earlier
+                .resolvers(new Query(linkRepository))
+                .build()
+                .makeExecutableSchema();
+    }
+}
